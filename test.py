3a30d6d3b070170d9d0ebc903df11a07a3cea24c
Terry Allitt <tallitt88@gmail.com>
2020-11-25 11:30:08 +0000

    Add logic to ask for age as input

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 def say_hello(name):
     print('Hello ' + name + '!')
+    age = input('How old are you?\n')
+    print('Wow! You are ' + str(age) + ' years old? Cool Beans!')
 
 
 say_hello('Tay Tay')
