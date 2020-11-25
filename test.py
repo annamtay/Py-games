7485286bd973733d67c8c4f74553f94112aefc4c
annamtay <anna.m.taylor25@googlemail.com>
2020-11-25 11:39:54 +0000

    Remove test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def say_hello(name):
-    print('Hello ' + name + '!')
-    age = input('How old are you?\n')
-    print('Wow! You are ' + str(age) + ' years old? Cool Beans!')
-
-
-say_hello('Tay Tay')
