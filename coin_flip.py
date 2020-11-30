7783c728a9c4137ec61d84a55bcaef955b4692c8
annamtay <anna.m.taylor25@googlemail.com>
2020-11-30 09:00:42 +0000

    Function to check yes or no working - game working - two not yet connected

--- coin_flip.py
+++ coin_flip.py
@@ -4,7 +4,25 @@ import time
 # define
 POSITIVE_RESPONSES = ['Yes', 'yes', 'Sure', 'Why not', 'Yep', 'Yes please']
 NEGATIVE_RESPONSES = ['Nope', 'No', 'no', 'Nah', 'No way', 'No thanks']
+ACCEPTED_RESPONSES = POSITIVE_RESPONSES + NEGATIVE_RESPONSES
 COIN_OPTIONS = ['Heads', 'Tails']
+
+
+# Function to verify if a user wants to play a game
+def yes_or_no():
+    answers = []
+    answer = input(print("Would you like to play a game?\n"))
+    answers.append(answer)
+    while answer not in ACCEPTED_RESPONSES:
+        print("Sorry I don\'t understand your answer.")
+        answer = input(print("Yes or no? \n"))
+        answers.append(answer)
+        if answers[-1] in ACCEPTED_RESPONSES:
+            break
+    if answers[-1] in POSITIVE_RESPONSES:
+        print("Woohoo let\'s play")
+    elif answers[-1] in NEGATIVE_RESPONSES:
+        return "FINE GAME OVER"
 
 
 # function to flip the coin and return winner or loser
@@ -26,19 +44,18 @@ def coin_flip_game(user_h_or_t):
 # get user input to play & strip user input of whitespace
 # get user input of 'Heads or Tails'
 def starting_the_game():
-    user_h_or_t = 0
     decision1 = input("Would you like to play a game? \n")
     decision1_stripped = decision1.strip()
     if decision1_stripped in NEGATIVE_RESPONSES:
         return "Oh no - that\'s a shame! Maybe next time!"
     elif decision1_stripped in POSITIVE_RESPONSES:
         user_h_or_t = input("Woohoo! Heads or Tails? \n")
-        if user_h_or_t not in COIN_OPTIONS:
-            print("I don\'t understand your answer.")
-        elif user_h_or_t in COIN_OPTIONS:
+        if user_h_or_t in COIN_OPTIONS:
             print("You guessed " + user_h_or_t)
             time.sleep(1)
             coin_flip_game(user_h_or_t)
+        elif user_h_or_t not in COIN_OPTIONS:
+            print("I don\'t understand your answer.")
     else:
         return("I don\'t understand your answer. Let\'s try again in the future")
     return "Thanks for playing!"
@@ -47,7 +64,7 @@ def starting_the_game():
 print(starting_the_game())
 
 
-# #fucntion to count coin flips - take a list of 'Heads' or 'Tails' and will return the count
+# #function to count coin flips - take a list of 'Heads' or 'Tails' and will return the count
 # def counting_coin_flips(past_flips):
 #     count_heads = 0
 #     count_tails = 0
